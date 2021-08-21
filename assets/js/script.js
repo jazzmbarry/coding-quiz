3ca6e2c2c01c85dab57864b4fadfedccb2e5ebd7
jazzmbarry <jazzmbarry@gmail.com>
2021-08-21 17:10:02 -0600

    Create Questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@ var questionsEl = document.querySelector("#questions")
 var choicesEl = document.querySelector("#choices")
 var answerEl = document.querySelector("#answer")
 var timerEl = document.querySelector("#timer")
-var timer = 75
-var qAndA = [
-    {q: "This is the first quesiton", a: "This is the first answer"},
-    {q: "This is the second quesiton", a: "This is the second answer"},
-    {q: "This is the third quesiton", a: "This is the third answer"},
-    {q: "This is the fourth quesiton", a: "This is the fourth answer"},
-    {q: "This is the fifth quesiton", a: "This is the fifth answer"}
+var timer = 1
+var qAC = [
+    {q: "JavaScript has a file extension of ____", a: {a:".java", b:".js", c:".xml", d:".javascript"}, c: 'b'},
+    {q: "Inside which HTML element do we put JavaScript?", a: {a:"scripting", b:"script", c:"JavaScript", d:"js"}, c: "b"},
+    {q: "var numbers = [1,2,3,] is an example of:", a: {a:"Array", b:"Function", c:"Object", d:"Method"} , c: "a"},
+    {q: "Where is the correct place to insert JavaScript into your HTML project?", a: {a:"Beginning of the <head>", b:"Middle of the <header>", c:"End of the <body>", d:"Before the <head>"}, c: "c"},
+    {q: "How can you insert a comment?", a: {a:"/*Comment", b:"/Comment", c:"//This comment", d:"<!-- Comment --!>"}, c: "c"}
 ]
 
 var countdown = function(){
@@ -29,9 +29,18 @@ var startCountdown = function(){
     setTimeout(countdown, 1000)
 }
 
-startQuizEl.addEventListener("click", function() {
-    if (timer !== 0){
-        startCountdown();
+var gameFlow = function(){
+    
+    for (var i=0; i < qAndA; i++) {
+        questionsEl.replaceWith(qAC[i].q)
     }
+}
 
-})
+var startGame = function(){
+    startCountdown()
+    gameFlow
+}
+
+
+startQuizEl.addEventListener("click", startGame()
+);
